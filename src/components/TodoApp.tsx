5fa88d236c6e7b15d18dfe9b29e1b5c1dcb3b5ac
{cocoide} <{cocoide.com@gmail.com}>
2022-11-10 19:19:51 +0900

    design TODO form

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -7,26 +7,44 @@ type Todo = {
 export const TodoApp: FC = () => {
     const [text, setText] = useState("")
     const [todos, setTodos] = useState<Todo[]>([])
+    const handleOnSubmit = () => {
+        // TODOが未入力なら、TODOを新規作成
+        if (!text) return
+        const newTodo: Todo = {
+            value: text
+        }
+        // 以下のスプレッド構文の詳細
+        //  const copyTodos = todos.slice();
+        // copyTodos.unshift(newTodo);
+        //  setTodos(copyTodos);
+        setTodos([newTodo, ...todos])
+        //入力をクリア
+        setText("")
+    }
 
     return (
-        <div>
-            <form onSubmit={(e) => e.preventDefault()}
-                className="flex flex-raw">
+        <div className="rounded shadow-inner">
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault()
+                    handleOnSubmit()
+                }}
+                className="flex flex-raw bg-white h-10">
 
                 <input
                     type="text"
+                    placeholder="NEW TODO ++ "
                     value={text}
                     onChange={(e) => setText(e.target.value)}
-                    className="outline-purple-200"
+                    className="outline-purple-200 text-center"
                 />
-                <PlusCircleIcon className="h-6 w-6 ml-1 text-purple-300">
-                    <input
-                        type="submit"
-                        value=""
-                        onSubmit={(e) => e.preventDefault()}
-                    />
-                </PlusCircleIcon>
+                {/* <PlusCircleIcon className="h-6 w-6 ml-1 text-purple-300" /> */}
+                <input
+                    type="submit"
+                    value="◉"
+                    onSubmit={(e) => e.preventDefault()}
+                    className="text-purple-300 h-10 w-10" />
             </form>
-        </div>
+        </div >
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,21 @@
 import Head from 'next/head'
 import { TodoApp } from '../components/TodoApp'
-
+import { ShareIcon } from '@heroicons/react/24/outline'
 
 export default function Home() {
 
   return (
     <div className='from-white via-purple-200 to-blue-200 h-screen bg-gradient-to-tr'>
       <Head>
-        <title>Share To Do</title>
+        <title>To Do Share</title>
         <meta name="description" content="Share To Do" />
       </Head>
 
 
       <div>
 
-        <h1 className="text-5xl p-10 text-center font-bold text-white"
-        >Share To Do</h1>
+        <h1 className="text-5xl p-10 text-center font-bold text-white place-items-center flex flex-row justify-center"
+        ><ShareIcon className='h-10 w-10 mr-3' />To Do Share</h1>
         <div className="h-80 w-80 m-auto bg-gray-50 rounded-md 
         flex justify-center place-items-center shadow-purple-300/50">
 
